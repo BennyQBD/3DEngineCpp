efdeace3eb394f169c9d765f77163770251c270b
BennyQBD <bennyqbdspam@gmail.com>
2014-05-27 17:08:57 -0400

    Included build Directory

--- _PushToGithub.sh
+++ _PushToGithub.sh
@@ -1,8 +1,8 @@
 #Clean up files that aren't wanted in the commit
 ./Unix-Clean.sh
 
-#Add .gitkeep so github includes build directory
-touch ./build/.gitkeep
+#Copy README.md to build directory so github includes it
+cp README.md ./build
 
 #Add everything to a new commit and push it to github
 git add -A
